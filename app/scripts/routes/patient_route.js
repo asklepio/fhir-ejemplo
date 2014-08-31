8b767e751ca1d4133e01da7db8bb18cdad6581bb
asklepio <adolfo.carpio@safehis.com>
2014-08-31 12:56:12 -0400

    Better patient route:

--- app/scripts/routes/patient_route.js
+++ app/scripts/routes/patient_route.js
@@ -1,3 +1,41 @@
+function parseSinglePatientRecord(record) {
+	if ('id' in record) {
+		var idArray = record.id.split('/');
+		var givenName = '';
+		var familyName = '';
+		var patientSex = '';
+		var patientBirthDate = '';
+		if ('content' in record) {
+			if ('name' in record.content && record.content.name instanceof Array) {
+				givenName = record.content.name[0].given;
+				familyName = record.content.name[0].family;
+			} else {
+				givenName = 'SIN DATOS';
+				familyName = 'SIN DATOS';
+			}
+			if ('gender' in record.content && 'coding' in record.content.gender && record.content.gender.coding instanceof Array) {
+				patientSex = record.content.gender.coding[0].code;
+			} else {
+				patientSex = 'SIN DATOS';
+			}
+			if ('birthDate' in record.content) {
+				patientBirthDate = record.content.birthDate;
+			} else {
+				patientBirthDate = 'SIN DATOS';
+			}
+			return Fhir.Patient.create({
+				id: idArray[idArray.length - 1],
+				given: givenName,
+				family: familyName,
+				sex: patientSex,
+				birthDate: patientBirthDate
+			});
+		}
+	}
+
+	return undefined;
+}
+
 Fhir.PatientRoute = Ember.Route.extend({
 	model: function(patient) {
 		var newPatient;
